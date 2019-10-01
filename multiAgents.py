fa90a842e470456ce46685fbdedb0f00753d1473
SeungJu <sgmath12@gmail.com>
2019-10-01 16:55:13 -0400

    pass all test cases

--- multiAgents.py
+++ multiAgents.py
@@ -335,18 +335,15 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
 
       return v,None
     
-    def Value(self,gameState,depth,agentNumber,action = None):
-      if gameState.isWin() or gameState.isLose():
-        return gameState.getScore(),action
-      
-      if agentNumber == 0 and depth ==0:
-        return gameState.getScore(),action
+    def Value(self,gameState,depth,agentNumber,action = None):      
+      if (agentNumber == 0 and depth ==0) or (gameState.isWin() or gameState.isLose()):
+        return self.evaluationFunction(gameState),action
+        #return gameState.getScore(),action
       elif agentNumber == 0 : # nextAgent is MAX
         return self.maxValue(gameState,depth,agentNumber)
       else: #nextAgent is MIN
         return self.expValue(gameState,depth,agentNumber)
     
-
     def getAction(self, gameState):
         """
           Returns the expectimax action using self.depth and self.evaluationFunction
@@ -366,57 +363,42 @@ def betterEvaluationFunction(currentGameState):
       DESCRIPTION: <write something here so we know what you did>
       Return Score
     """
-    print ("Hihihihi")
 
     # Useful information you can extract from a GameState (pacman.py)
-    successorGameState = currentGameState.generatePacmanSuccessor(action)
-    newPos = successorGameState.getPacmanPosition()
-    newFood = successorGameState.getFood()
-    newGhostStates = successorGameState.getGhostStates()
-    newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-    Walls = successorGameState.getWalls()
+    #return currentGameState.getScore()
+
+    '''
+'generateSuccessor', 'getAndResetExplored', 'getCapsules', 'getFood',
+ 'getGhostPosition', 'getGhostPositions', 'getGhostState', 'getGhostStates', 
+ 'getLegalActions', 'getLegalPacmanActions', 'getNumAgents', 'getNumFood',
+  'getPacmanPosition', 'getPacmanState', 'getScore',
+ 'getWalls', 'hasFood', 'hasWall', 'initialize', 'isLose', 'isWin
+    '''
+
+    #print (dir(currentGameState))
+    Pos = currentGameState.getPacmanPosition()
+    numFoods = currentGameState.getNumFood()
+    Walls = currentGameState.getWalls()
+    Foodgrid = currentGameState.getFood()
+    FoodMinDistance = float('inf')
+    # pass : .7, .2, .1
+    w1,w2,w3= (.7,.2,.1)
+    ghostDistances = 0
+    ghostMinDistance = float('inf')
+
+    for ghostAgentNumber in range(1,currentGameState.getNumAgents()):
+        ghostPos = currentGameState.getGhostPosition(ghostAgentNumber)
+        #if ghostDistances > manhattanDistance(Pos,ghostPos):
+        ghostDistances += manhattanDistance(Pos,ghostPos)
+        if ghostMinDistance > manhattanDistance(Pos,ghostPos):
+          ghostMinDistance = manhattanDistance(Pos,ghostPos)
 
 
-    "*** YOUR CODE HERE ***"
-    # manhatten distance
-    Ghost_distances = []
-    Sign_Ghosts = []
-    Foods_distance = []
-    Foods = 0
-    newScore = 0
-
-    for i in range(newFood.width):
-      for j in range(newFood.height):
-        if newFood[i][j]:
-          Foods_distance.append(abs(newPos[0] - i) + abs(newPos[1] - j))
-          Foods += 1
-
-    Foods_distance.sort()
-    # float division
-
-    # minus weight if ghost can attack, plus weight if pacman can eat scared ghost
-    for i , ghost_state in enumerate(newGhostStates):
-        newGhostPos = ghost_state.getPosition()
-        Ghost_distances.append(abs(newPos[0] - newGhostPos[0]) + abs(newPos[1] - newGhostPos[1]))
-        if Ghost_distances[i] <= newScaredTimes[i]:
-          Sign_Ghosts.append(1)
-        else :
-          Sign_Ghosts.append(-1) 
-
-    for i in range(len(newGhostStates)):
-      if Ghost_distances[i] > 2 or Sign_Ghosts[i] == 1:
-        pass
-      else:
-        return -10000
-
-    newScore += (newFood.width*newFood.height-Foods) *(newFood.width+newFood.height)
-
-    # find Nearset foods
     visited = {}
     dx = [-1,0,1,0]
     dy = [0,1,0,-1]
-    Queue = [(newPos,0)]
-    visited[newPos] = True
+    Queue = [(Pos,0)]
+    visited[Pos] = True
     head = 0
     
     tail = 1
@@ -424,7 +406,7 @@ def betterEvaluationFunction(currentGameState):
       cur,dist = Queue[head]
       head += 1
 
-      if newFood[cur[0]][cur[1]]:
+      if Foodgrid[cur[0]][cur[1]]:
         break
       for d in range(4):
         next_cur = (cur[0]+dx[d],cur[1]+dy[d])
@@ -432,12 +414,19 @@ def betterEvaluationFunction(currentGameState):
           visited[next_cur] = True
           Queue.append((next_cur,dist + 1))
           tail += 1
-        
-    if Foods > 0:
-      newScore += -dist + (newFood.width + newFood.height)
+
+    if ghostMinDistance == 0:
+      return -1000
+
+    score = 0
+    score += (Foodgrid.width*Foodgrid.height-numFoods) *(Foodgrid.width+Foodgrid.height)
+    if numFoods > 0:
+      score += -dist + (Foodgrid.width + Foodgrid.height)
     else :
-      newScore += newFood.width + newFood.height
-    return 0
+        score += Foodgrid.width + Foodgrid.height
+    
+    return w1*currentGameState.getScore() +w2*score + w3*(ghostDistances)
+
     
 # Abbreviation
 better = betterEvaluationFunction
